a7c2fb5067becd62797c8652e52aeab5fb369536
NazwaYaqzhania <106669446+NazwaYaqzhania@users.noreply.github.com>
2022-12-03 19:05:39 +0700

    Add files via upload

---
+++ MainApp.java
@@ -0,0 +1,22 @@
+public class MainApp {
+    public static void main(String[] args) throws Exception {
+        Perangkat perangkats = new Perangkat("hp", 7, 3.3f);
+        perangkats.informasi();
+
+        System.out.println();
+
+        Handphone hp = new Handphone(false, "sunscreen", 8, 2.2f);
+        hp.informasi();
+        hp.telfon(858834);
+        hp.kirimSMS(25883);
+        hp.multiSMS(12345, 5789);
+
+        System.out.println();
+
+        Laptop laptop = new Laptop("Seal", 16, 3.2f, false);
+        laptop.informasi();
+        laptop.bukaGame("MobileLegend");
+        laptop.kirimEmail("nanazimtyaz@gmail.com");
+        laptop.multiEmail("nanazimtyaz@mail.com", "nanazimtyaz@mail.com");
+    }
+}
